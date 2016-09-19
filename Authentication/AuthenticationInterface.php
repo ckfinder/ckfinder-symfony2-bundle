059fe69e2734a2994864da3829b534b265d40f40
mkesicki <marc_he_w@poczta.onet.pl>
2016-09-19 17:46:39 +0400

    Fix namespace

--- Authentication/AuthenticationInterface.php
+++ Authentication/AuthenticationInterface.php
@@ -12,7 +12,6 @@
 namespace CKSource\Bundle\CKFinderBundle\Authentication;
 
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-// use CKSource\CKFinder\Authentication\AuthenticationInterface as BaseAuthenticationInterface;
 
 /**
  * Interface for the CKFinder authentication service.
